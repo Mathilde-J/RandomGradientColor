1aef0b36b2af679983f541201bd81f5e4c31abcf
Mathilde-J <mthldjanssen@gmail.com>
2022-03-04 16:48:54 +0100

    stroe vanilla

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 // == Imports
+import store from 'src/store';
+
 import { randomHexColor, generateSpanColor } from './utils';
 
-// == State
-const state = {
-  firstColor: '#e367a4',
-  lastColor: '#48b1f3',
-  direction: '90deg',
-  nbColors: 0,
-};
+/* Objectif : faire fonctionner le bouton "random last". Plan d'action :
+x dispatch l'action (src/index.js, handler du bouton)
+- dans le reducer, mettre en place le traitement pour cette action
+*/
+
+// console.log(store.getState());
 
 // == Rendu dans le DOM
 function renderNbColors() {
-  const { nbColors } = state;
+  const { nbColors } = store.getState();
 
   document.getElementById('nbColors').innerHTML = `
     ${nbColors} couleur(s) générée(s)
   `;
 }
-function renderGradient() {
-  const { direction, firstColor, lastColor } = state;
 
+function renderGradient() {
+  const { direction, firstColor, lastColor } = store.getState();
+
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/gradient/linear-gradient()
   document.getElementById('gradient').style.background = `
     linear-gradient(${direction},${firstColor},${lastColor})
   `;
 }
+
 function renderColors() {
-  const { firstColor, lastColor } = state;
+  const { firstColor, lastColor } = store.getState();
 
   const firstSpan = generateSpanColor(firstColor);
   const lastSpan = generateSpanColor(lastColor);
@@ -40,49 +44,91 @@ renderNbColors();
 renderGradient();
 renderColors();
 
+// abonnement aux changements du state : en cas d'appel à dispatch, la callback
+// fournie en argument sera automatiquement appelée
+store.subscribe(() => {
+  console.log('le state a changé');
+
+  // on refait l'affichage
+  renderNbColors();
+  renderGradient();
+  renderColors();
+});
+
 // == Controls
 document.getElementById('randAll')
   .addEventListener('click', () => {
     // debug
     console.log('Random all colors');
+
     // data
-    state.nbColors += 2;
-    state.firstColor = randomHexColor();
-    state.lastColor = randomHexColor();
+    // on dispatch une action CHANGE_FIRST_COLOR et une action CHANGE_LAST_COLOR
+    const action = {
+      type: 'CHANGE_FIRST_COLOR',
+      color: randomHexColor(),
+    };
+    store.dispatch(action);
+
+    // on peut ausi écrire sans variable intermédiaire
+    store.dispatch({
+      type: 'CHANGE_LAST_COLOR',
+      color: randomHexColor(),
+    });
+
     // ui
-    renderNbColors();
-    renderGradient();
-    renderColors();
   });
 
 document.getElementById('randFirst')
   .addEventListener('click', () => {
-    state.nbColors += 1;
-    state.firstColor = randomHexColor();
-    renderNbColors();
-    renderGradient();
-    renderColors();
+    const newColor = randomHexColor();
+
+    /*
+    une action c'est un objet qui contient au minimum une propriété "type".
+    Il peut contenir aussi d'autres propriétés : on appelle ces informations
+    le payload ("chargement") de l'action
+    */
+    const action = {
+      type: 'CHANGE_FIRST_COLOR',
+      color: newColor,
+    };
+    store.dispatch(action);
   });
 
 document.getElementById('randLast')
   .addEventListener('click', () => {
-    state.nbColors += 1;
-    state.lastColor = randomHexColor();
-    renderNbColors();
-    renderGradient();
-    renderColors();
+    const action = {
+      type: 'CHANGE_LAST_COLOR',
+      color: randomHexColor(),
+    };
+    store.dispatch(action);
   });
 
 document.getElementById('toLeft')
   .addEventListener('click', () => {
-    state.direction = '270deg';
-    renderGradient();
-    renderColors();
+    // une action c'est une intention
+    // "store, change la direction pour aller vers la gauche"
+
+    // une action c'est un objet qui contient l'intention dans sa propriété "type"
+
+    // on veut appliquer un changement sur le state : on envoie une action au
+    // store (dispatch), le store transmettra l'action au reducer avec aussi le
+    // state actuel. En fait, le reducer se débrouillera pour traduire l'intention
+    // en modifications sur le state
+
+    const action = {
+      type: 'DIRECTION_TO_LEFT',
+    };
+
+    // on envoie l'action vers le store => dispatch
+    store.dispatch(action);
   });
 
 document.getElementById('toRight')
   .addEventListener('click', () => {
-    state.direction = '90deg';
-    renderGradient();
-    renderColors();
+    const action = {
+      type: 'DIRECTION_TO_RIGHT',
+    };
+
+    // on envoie l'action vers le store => dispatch
+    store.dispatch(action);
   });
---
+++ src/store/reducer.js
@@ -0,0 +1,95 @@
+// reducer : fonction qui fournit le nouveau state à partir du state actuel et
+// d'une action. C'est l'assistant du store (en vrai c'est un peu lui qui fait tout
+// le boulot, le store se contente de lui transmettre action et state, puis il
+// récupère le nouveau state et remplace le state d'avant)
+
+// le reducer va traduire l'action en changements des informations du state
+
+// action : c'est pour demander une modification sur le state, par exemple
+// "passe le nombre de couleurs à 5"
+
+const initialState = {
+  firstColor: '#e367a4',
+  lastColor: '#48b1f3',
+  direction: '90deg',
+  nbColors: 0,
+};
+
+// eslint-disable-next-line arrow-body-style
+const reducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case 'DIRECTION_TO_LEFT':
+      // on va créer une copie du state, et modifier la direction dans cette copie
+      // => on crée un nouvel objet dans lequel on déverse toutes les informations
+      // du state actuel, et on en profite pour faire les modifications
+      return {
+        // on déverse toutes les infos du state actuel
+        ...state,
+        // on applique les modifications
+        direction: '270deg',
+      };
+
+    case 'DIRECTION_TO_RIGHT':
+      return {
+        ...state,
+        direction: '90deg',
+      };
+
+    case 'CHANGE_FIRST_COLOR':
+      return {
+        ...state,
+        // on peut se servir d'une information du state actuel
+        nbColors: state.nbColors + 1,
+        firstColor: action.color,
+      };
+
+    case 'CHANGE_LAST_COLOR':
+      return {
+        ...state,
+        // on peut se servir d'une information du state actuel
+        nbColors: state.nbColors + 1,
+        lastColor: action.color,
+      };
+
+      // case 'CHANGE_ALL_COLOR':
+      //   return {
+      //     ...state,
+      //     // on peut se servir d'une information du state actuel
+      //     nbColors: state.nbColors + 2,
+      //     lastColor: action.color.colorFirst,
+      //     firstColor: action.color.colorLast,
+      //   };
+
+    default: return state;
+  }
+};
+export default reducer;
+
+// quand le store est créé, il ne contient pas encore de state. Il va faire appel
+// au reducer avec un state vide (undefined) et une action spéciale "INIT"
+
+// paramètre par défaut pour une fonction : si la fonction est appelée avec ce
+// paramètre qui n'a pas de valeur, alors dans le corps de la fonction la valeur
+// par défaut sera utilisée pour ce paramètre
+
+// si on appelle la fonction reducer avec un paramètre state undefined :
+// reducer(undefined, "INIT")
+// => quand on écrit "return state", en fait ça va retourner initialState
+
+/*
+if (laVariable === 1) {
+  console.log('A');
+} else if (laVariable === 2) {
+  console.log('B');
+} else {
+  console.log('C');
+}
+
+switch : aiguillage entre des traitements par rapport à la valeur d'une variable
+
+switch (laVariable) {
+  case 1 : console.log('A');
+  case 2 : console.log('B');
+  default: console.log('C');
+}
+*/
